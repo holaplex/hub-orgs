5b320416a1470b47cf01529ff691b46312611d00
Abdul Basit <baronbasit@gmail.com>
2022-12-23 19:30:22 +0500

    fix invites down migration

--- migration/src/m20221219_141929_create_invites_table.rs
+++ migration/src/m20221219_141929_create_invites_table.rs
@@ -93,6 +93,10 @@ impl MigrationTrait for Migration {
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
             .drop_table(Table::drop().table(Invites::Table).to_owned())
+            .await?;
+
+        manager
+            .drop_type(Type::drop().if_exists().name(Status::Type).to_owned())
             .await
     }
 }
